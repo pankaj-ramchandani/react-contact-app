a631e98d64f3651d3db45c4c7cd407349dfe047e
Pankaj Ramchandani <pankaj@edmingle.com>
2022-04-25 09:46:03 +0530

    added local storage logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
-function App() {
-  const contacts = [
-    {
-      id: "1",
-      name: "Dipesh",
-      email: "malvia@gmail.com",
-    },
-    {
-      id: "2",
-      name: "Nikesh",
-      email: "nikesh@gmail.com",
-    },
-  ];
+function App() {  
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, contact]);
+  }
+  useEffect(() => {
+    const retrievedContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(retrievedContacts)
+      setContacts(retrievedContacts);
+  },[]);
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  },[contacts]);
   return (
     <div className="ui container">
       <Header />
-      <AddContact />
+      <AddContact addContactHandler={addContactHandler}/>
       <ContactList contacts={contacts}/>
     </div>
   );
